da9c9748cb26ef49d78936af3ea79b1c014cb679
ufukgulec <oug505@gmail.com>
2020-04-22 18:16:51 +0300

    Project added.

---
+++ BilgisayarGrafikleri2-SimpleOpenGl/ödev/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ödev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.buttonX = new System.Windows.Forms.Button();
+            this.buttonY = new System.Windows.Forms.Button();
+            this.buttonZ = new System.Windows.Forms.Button();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.labelZ = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(0, 0);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(674, 266);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.Click += new System.EventHandler(this.simpleOpenGlControl1_Click);
+            this.simpleOpenGlControl1.DoubleClick += new System.EventHandler(this.simpleOpenGlControl1_DoubleClick);
+            // 
+            // buttonX
+            // 
+            this.buttonX.Location = new System.Drawing.Point(532, 12);
+            this.buttonX.Name = "buttonX";
+            this.buttonX.Size = new System.Drawing.Size(75, 23);
+            this.buttonX.TabIndex = 1;
+            this.buttonX.Text = "buttonX";
+            this.buttonX.UseVisualStyleBackColor = true;
+            this.buttonX.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonKeyDown);
+            this.buttonX.KeyUp += new System.Windows.Forms.KeyEventHandler(this.buttonKeyUp);
+            this.buttonX.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonX_MouseClick);
+            // 
+            // buttonY
+            // 
+            this.buttonY.Location = new System.Drawing.Point(532, 41);
+            this.buttonY.Name = "buttonY";
+            this.buttonY.Size = new System.Drawing.Size(75, 23);
+            this.buttonY.TabIndex = 2;
+            this.buttonY.Text = "buttonY";
+            this.buttonY.UseVisualStyleBackColor = true;
+            this.buttonY.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonKeyDown);
+            this.buttonY.KeyUp += new System.Windows.Forms.KeyEventHandler(this.buttonKeyUp);
+            this.buttonY.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonY_MouseClick);
+            // 
+            // buttonZ
+            // 
+            this.buttonZ.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonZ.Location = new System.Drawing.Point(532, 70);
+            this.buttonZ.Name = "buttonZ";
+            this.buttonZ.Size = new System.Drawing.Size(75, 23);
+            this.buttonZ.TabIndex = 3;
+            this.buttonZ.Text = "buttonZ";
+            this.buttonZ.UseVisualStyleBackColor = false;
+            this.buttonZ.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonKeyDown);
+            this.buttonZ.KeyUp += new System.Windows.Forms.KeyEventHandler(this.buttonKeyUp);
+            this.buttonZ.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonZ_MouseClick);
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(613, 17);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(13, 13);
+            this.labelX.TabIndex = 4;
+            this.labelX.Text = "0";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(613, 46);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(13, 13);
+            this.labelY.TabIndex = 5;
+            this.labelY.Text = "0";
+            // 
+            // labelZ
+            // 
+            this.labelZ.AutoSize = true;
+            this.labelZ.Location = new System.Drawing.Point(613, 75);
+            this.labelZ.Name = "labelZ";
+            this.labelZ.Size = new System.Drawing.Size(13, 13);
+            this.labelZ.TabIndex = 6;
+            this.labelZ.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(532, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Obje değiştir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 266);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelZ);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.buttonZ);
+            this.Controls.Add(this.buttonY);
+            this.Controls.Add(this.buttonX);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.myPaint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Button buttonX;
+        private System.Windows.Forms.Button buttonY;
+        private System.Windows.Forms.Button buttonZ;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Label labelZ;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BilgisayarGrafikleri2-SimpleOpenGl/ödev/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ödev
+{
+    public partial class Form1 : Form
+    {
+        private bool invert = false;
+
+        private int list;
+
+        private float alfa = 0;
+
+        private float beta = 0;
+        
+        private float gamma = 0;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            simpleOpenGlControl1.InitializeContexts();
+
+            int height = simpleOpenGlControl1.Height;
+
+            int width = simpleOpenGlControl1.Width;
+
+            Gl.glEnable(Gl.GL_LIGHTING);
+
+            Gl.glEnable(Gl.GL_LIGHT0);
+
+            float[] light_pos = new float[3] { 1, 0.5F, 1 };
+
+            Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light_pos);
+
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+
+            Gl.glClearColor(0, 0, 0, 1);
+
+
+
+            Gl.glViewport(0, 0, width, height);
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+
+            Gl.glLoadIdentity();
+
+            Glu.gluPerspective(45.0f, (double)width / (double)height, 0.01f, 5000.0f);
+
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+
+            create();
+        }
+        public void create()
+        {
+
+            Glu.GLUquadric quadratic = Glu.gluNewQuadric();
+
+            Glu.gluQuadricNormals(quadratic, Glu.GLU_SMOOTH);
+
+            Glu.gluQuadricDrawStyle(quadratic, Glu.GLU_LINE);
+
+            Glu.gluQuadricOrientation(quadratic,
+                 Glu.GLU_OUTSIDE);
+
+
+
+            list = Gl.glGenLists(1);
+
+            Gl.glNewList(list, Gl.GL_COMPILE);
+
+            Glu.gluSphere(quadratic,3, 32, 32);//pikselle
+
+            Gl.glEndList();
+
+        }
+        
+        private void myPaint(object sender, PaintEventArgs e)
+        {
+
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT |
+                 Gl.GL_DEPTH_BUFFER_BIT);
+
+            Gl.glLoadIdentity();
+
+            Glu.gluLookAt(5, 5, 5, 0, 0, 0, 0, 1, 0);//sağ sol 0 0 0
+
+             Gl.glRotatef(alfa,1, 0, 0);
+
+            Gl.glRotatef(beta, 0, 1, 0);
+
+            Gl.glRotatef(gamma, 0, 0, 1);
+
+            Gl.glCallList(list);
+
+        }
+
+        private void buttonX_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            if (invert)
+            { // Shift basılı ise ters döndür
+            
+                alfa -= 5;
+            }
+
+            else
+
+                alfa += 5;
+
+            alfa = (alfa + 360) % 360; // Mod işlemi: 0-355
+
+            labelX.Text = alfa.ToString();
+
+            simpleOpenGlControl1.Refresh();
+        }
+
+        private void buttonY_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (invert)
+
+                beta -= 5;
+
+            else
+
+                beta += 5;
+
+            beta = (beta + 360) % 360; // Mod işlemi: 0-355
+
+            labelY.Text = beta.ToString();
+
+            simpleOpenGlControl1.Refresh();
+        }
+
+        private void buttonZ_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (invert)
+
+                gamma -= 5;
+
+            else
+
+                gamma += 5;
+
+            gamma = (gamma + 360) % 360; // Mod işlemi: 0-355
+
+            labelZ.Text = gamma.ToString();
+
+            simpleOpenGlControl1.Refresh();
+        }
+
+        private void buttonKeyDown(object sender, KeyEventArgs e)
+        {
+            invert = false;
+
+            if (e.KeyCode == Keys.ShiftKey)
+
+                invert = true;
+        }
+
+        private void buttonKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ShiftKey)
+
+                invert = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                timer1.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //gamma = (gamma+ 5) % 360;
+            //labelZ.Text = la.ToString();
+            //Form1.ActiveForm.Refresh();
+            //simpleOpenGlControl1.Refresh();
+            //gamma += 5;
+            gamma = (gamma + 6) % 360; // Mod işlemi: 0-355
+            labelZ.Text = gamma.ToString();
+            
+            simpleOpenGlControl1.Refresh();
+        }
+
+       
+
+       private void simpleOpenGlControl1_Click(object sender, EventArgs e)
+       {
+           timer1.Enabled = true;
+          
+       }
+
+       private void simpleOpenGlControl1_DoubleClick(object sender, EventArgs e)
+       {
+           timer1.Enabled = false;
+           
+       }
+
+       private void button1_Click_1(object sender, EventArgs e)
+       {
+           
+       }
+
+       
+
+      
+
+       
+    }
+}
