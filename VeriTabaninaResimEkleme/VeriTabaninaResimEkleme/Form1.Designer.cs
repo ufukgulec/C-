957259259e02d518e713bc7d6a9ce73264af974b
ufukgulec <oug505@gmail.com>
2020-09-30 23:12:04 +0300

    Project Added.

---
+++ VeriTabaninaResimEkleme/VeriTabaninaResimEkleme/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace VeriTabaninaResimEkleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text_ad = new System.Windows.Forms.TextBox();
+            this.text_soyad = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.text_resim = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dbo_kayitDataSet = new VeriTabaninaResimEkleme.Dbo_kayitDataSet();
+            this.kayitTablosuBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.kayitTablosuTableAdapter = new VeriTabaninaResimEkleme.Dbo_kayitDataSetTableAdapters.kayitTablosuTableAdapter();
+            this.adiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soyadiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.resimDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbo_kayitDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kayitTablosuBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyisim";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fotoğrafı:";
+            // 
+            // text_ad
+            // 
+            this.text_ad.Location = new System.Drawing.Point(61, 29);
+            this.text_ad.Name = "text_ad";
+            this.text_ad.Size = new System.Drawing.Size(143, 20);
+            this.text_ad.TabIndex = 3;
+            // 
+            // text_soyad
+            // 
+            this.text_soyad.Location = new System.Drawing.Point(61, 56);
+            this.text_soyad.Name = "text_soyad";
+            this.text_soyad.Size = new System.Drawing.Size(143, 20);
+            this.text_soyad.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(222, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 21);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fotoğrafı";
+            // 
+            // text_resim
+            // 
+            this.text_resim.Location = new System.Drawing.Point(61, 85);
+            this.text_resim.Name = "text_resim";
+            this.text_resim.Size = new System.Drawing.Size(108, 20);
+            this.text_resim.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(175, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(29, 20);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.adiDataGridViewTextBoxColumn,
+            this.soyadiDataGridViewTextBoxColumn,
+            this.resimDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.kayitTablosuBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 190);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(344, 188);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dbo_kayitDataSet
+            // 
+            this.dbo_kayitDataSet.DataSetName = "Dbo_kayitDataSet";
+            this.dbo_kayitDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // kayitTablosuBindingSource
+            // 
+            this.kayitTablosuBindingSource.DataMember = "kayitTablosu";
+            this.kayitTablosuBindingSource.DataSource = this.dbo_kayitDataSet;
+            // 
+            // kayitTablosuTableAdapter
+            // 
+            this.kayitTablosuTableAdapter.ClearBeforeFill = true;
+            // 
+            // adiDataGridViewTextBoxColumn
+            // 
+            this.adiDataGridViewTextBoxColumn.DataPropertyName = "adi";
+            this.adiDataGridViewTextBoxColumn.HeaderText = "adi";
+            this.adiDataGridViewTextBoxColumn.Name = "adiDataGridViewTextBoxColumn";
+            // 
+            // soyadiDataGridViewTextBoxColumn
+            // 
+            this.soyadiDataGridViewTextBoxColumn.DataPropertyName = "soyadi";
+            this.soyadiDataGridViewTextBoxColumn.HeaderText = "soyadi";
+            this.soyadiDataGridViewTextBoxColumn.Name = "soyadiDataGridViewTextBoxColumn";
+            // 
+            // resimDataGridViewTextBoxColumn
+            // 
+            this.resimDataGridViewTextBoxColumn.DataPropertyName = "resim";
+            this.resimDataGridViewTextBoxColumn.HeaderText = "resim";
+            this.resimDataGridViewTextBoxColumn.Name = "resimDataGridViewTextBoxColumn";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(61, 163);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 21);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Yenile";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(61, 109);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 21);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Temizle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 386);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.text_resim);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.text_soyad);
+            this.Controls.Add(this.text_ad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbo_kayitDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kayitTablosuBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox text_ad;
+        private System.Windows.Forms.TextBox text_soyad;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox text_resim;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private Dbo_kayitDataSet dbo_kayitDataSet;
+        private System.Windows.Forms.BindingSource kayitTablosuBindingSource;
+        private Dbo_kayitDataSetTableAdapters.kayitTablosuTableAdapter kayitTablosuTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soyadiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn resimDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ VeriTabaninaResimEkleme/VeriTabaninaResimEkleme/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace VeriTabaninaResimEkleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti=new SqlConnection(@"Data Source=DESKTOP\;Initial Catalog=Dbo_kayit;Integrated Security=True");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into kayitTablosu(adi, soyadi, resim) values (@p1,@p2,@p3)",baglanti);
+            komut.Parameters.AddWithValue("@p1",text_ad.Text);
+            komut.Parameters.AddWithValue("@p2", text_soyad.Text);
+            komut.Parameters.AddWithValue("@p3", text_resim.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Ekleme başarılı...");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+            text_resim.Text = openFileDialog1.FileName;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dbo_kayitDataSet.kayitTablosu' table. You can move, or remove it, as needed.
+            this.kayitTablosuTableAdapter.Fill(this.dbo_kayitDataSet.kayitTablosu);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter adaptor = new SqlDataAdapter("Select * from kayitTablosu", baglanti);
+            DataSet ds = new DataSet();
+            adaptor.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            baglanti.Close();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            button1.Enabled = false;
+            text_ad.Text = dataGridView1.Rows[dataGridView1.SelectedCells[0].RowIndex].Cells[0].Value.ToString();
+            text_soyad.Text = dataGridView1.Rows[dataGridView1.SelectedCells[0].RowIndex].Cells[1].Value.ToString();
+            text_resim.Text = dataGridView1.Rows[dataGridView1.SelectedCells[0].RowIndex].Cells[2].Value.ToString();
+            pictureBox1.ImageLocation = dataGridView1.Rows[dataGridView1.SelectedCells[0].RowIndex].Cells[2].Value.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            text_ad.Clear();
+            text_soyad.Clear();
+            text_resim.Clear();
+        }
+    }
+}
