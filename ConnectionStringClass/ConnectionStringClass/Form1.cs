a4b0410ec77a1550d9e0ae2367c06af395ee1540
ufukgulec <oug505@gmail.com>
2020-09-30 23:11:03 +0300

    Project Added.

---
+++ ConnectionStringClass/ConnectionStringClass/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace ConnectionStringClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlBaglantisi baglan = new SqlBaglantisi();
+            SqlDataAdapter adaptor = new SqlDataAdapter("Select * from kitap",baglan.baglanti());
+            DataSet ds = new DataSet();
+            adaptor.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+    }
+}
---
+++ ConnectionStringClass/ConnectionStringClass/SqlBaglantisi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ConnectionStringClass
+{
+    class SqlBaglantisi
+    {
+        public SqlConnection baglanti() {
+            SqlConnection baglan = new SqlConnection(@"Data Source=DESKTOP;Initial Catalog=kutuphane;Integrated Security=True");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
